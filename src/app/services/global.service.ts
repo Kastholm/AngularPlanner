b10ee5d3b508efab24f080f39e43b90733a875af
Marc <marc@aguardio.com>
2023-05-24 20:01:03 +0200

    Made app online

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class GlobalService {
-  public globalPath: string = 'http://localhost:4000';
-  //public globalPath: string = 'http://192.168.87.155:27019';
+  //public globalPath: string = 'http://localhost:4000';
+  public globalPath: string = 'http://192.168.87.155:27019';
 
   constructor() {}
 }
